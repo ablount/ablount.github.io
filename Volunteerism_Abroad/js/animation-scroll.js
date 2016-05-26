f59d116f6a78fc780997e6de3ad42f7276985cf4
ablount <ablount21@gmail.com>
2016-05-25 19:34:10 -0500

    va test

---
+++ Volunteerism_Abroad/js/animation-scroll.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+
+    
+    $(window).scroll(function () {
+
+        var verticalScroll = $(this).scrollTop();
+
+        if (verticalScroll >= 500) {
+
+            $('#left_pic').addClass('animated fadeInRight');
+            $('#left_pic').removeClass('hide_me');
+        }
+
+    });
+    
+        $(window).scroll(function () {
+
+        var verticalScroll = $(this).scrollTop();
+
+        if (verticalScroll >= 500) {
+
+            $('#right_pic').addClass('animated fadeInLeft');
+            $('#right_pic').removeClass('hide_me');
+        }
+
+    });
+    
+
+    $(window).scroll(function () {
+
+        var verticalScroll = $(this).scrollTop();
+
+        if (verticalScroll >= 800) {
+
+            $('#blue_photo').addClass('animated fadeInLeft');
+            $('#blue_photo').removeClass('hide_me');
+        }
+
+    });
+    
+    $(window).scroll(function () {
+
+        var verticalScroll = $(this).scrollTop();
+
+        if (verticalScroll >= 500) {
+
+            $('#class_photo').addClass('animated fadeInRight');
+            $('#class_photo').removeClass('hide_me');
+        }
+
+    });
+
+    $(window).scroll(function () {
+
+        var verticalScroll = $(this).scrollTop();
+
+        if (verticalScroll >= 1000) {
+
+            $('#contact_button').addClass('animated bounce');
+        }
+
+    });
+
+    $(window).scroll(function () {
+
+        var verticalScroll = $(this).scrollTop();
+
+        if (verticalScroll >= 1500) {
+
+            $('#my_faq').addClass('animated tada');
+        }
+
+    });
+
+
+
+});
